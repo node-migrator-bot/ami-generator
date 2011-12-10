70d55e664866ab4e81057299171aafe62399e6a6
Steve Campbell <steve@perfectapi.com>
2011-12-10 18:08:13 +0000

    update package version for republish

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var gen = require('./modules/generator/generator.js');
 var config = {   
         "root": "./scripts"
     ,   "baseAMI": "ami-a562a9cc"
-    ,   "scripts": ["ubuntu11.10/Node0.4.12", "ubuntu11.10/Node0.4.12/cloud9", "ubuntu11.10/NginxProxy"]
+    ,   "scripts": ["ubuntu11.10/AWS_API_tools", "ubuntu11.10/Node0.4.12/cloud9", "ubuntu11.10/NginxProxy", "ubuntu11.10/Node0.4.12/nide"]
     };
     
 gen.getImageUsingConfig(config, function(err, amiId) {
