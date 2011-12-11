2b8a6498c09f7b80abfaecd62fe1eea063b361c9
Steve Campbell <steve@perfectapi.com>
2011-12-11 01:20:20 +0000

    upping npm version again, publishing from github rather

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var gen = require('./modules/generator/generator.js');
 
 var config = {   
         "root": "./scripts"
-    ,   "baseAMI": "ami-a562a9cc"
+    ,   "baseAMI": "ami-bf62a9d6"
     ,   "scripts": ["ubuntu11.10/AWS_API_tools", "ubuntu11.10/Node0.4.12/cloud9", "ubuntu11.10/NginxProxy", "ubuntu11.10/Node0.4.12/nide"]
     };
     
