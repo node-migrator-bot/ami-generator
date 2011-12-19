4d259a5816b69b718ee848385f31cd75270f27cb
Steve Campbell <dukeytoo@gmail.com>
2011-12-19 14:44:21 -0600

    Improved help messages for bin

--- bin/amigen.js
+++ bin/amigen.js
@@ -7,6 +7,7 @@
 var program = require('commander');
 var gen = require('../modules/generator/generator.js');
 var path = require('path');
+var fs = require('fs');
 
 program
   .usage('[options] scripts')
@@ -27,13 +28,21 @@ program.ami = program.ami || 'ami-bf62a9d6';
 program.root = program.root || path.resolve(__dirname, '../scripts');
 program.scripts = program.args || [];
 
-if (program.scripts.length == 0) {
-	console.log('The scripts parameter is required, e.g. amigen ubuntu11.10/node-latest');
+if (!path.existsSync(program.root)) {
+	console.log();
+	console.log('Scripts path "' + program.root + '" does not exist');
 	process.exit(1);
 }
 
-if (!path.existsSync(program.root)) {
-	console.log('Scripts path "' + program.root + '" does not exist');
+if (program.scripts.length == 0) {
+	console.log();
+	console.log('The scripts parameter is required, e.g. amigen ubuntu11.10/node-latest.  The following scripts are available:');
+	console.log();
+
+	var paths = [];
+	walkPathSync(program.root, '', paths);
+	console.log(paths);
+	
 	process.exit(1);
 }
 
@@ -50,3 +59,19 @@ gen.getImageUsingConfig(config, function(err, amiId) {
         console.log('ok, done - amiId = ' + amiId);
     }
 });
+
+function walkPathSync(root, currentPath, results) {
+	results = results || [];
+	
+	var files = fs.readdirSync(path.resolve(root, currentPath));
+	for (var i=0;i<files.length;i++) {
+		var currentFileResolved = path.resolve(root, currentPath, files[i]);
+		var isDir = fs.statSync(currentFileResolved).isDirectory();
+		
+		if (isDir) {
+			var result = path.join(currentPath, files[i])
+			results.push(result);
+			walkPathSync(root, result, results);
+		}
+	}
+}
