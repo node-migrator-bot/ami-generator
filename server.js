563e91465aafc762d9c9f0384731fcead101130b
Steve Campbell <dukeytoo@gmail.com>
2011-12-22 21:44:12 -0600

    Altered server.js to use new perfectapi connect middleware

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 var perfectapi = require('./../perfectapi/api.js');
-var gen = require(__dirname + '/modules/generator/generator.js');
+var gen = require('./modules/generator/generator.js');
 var path = require('path');
+var express = require('express');
 
-perfectapi.rest(path.resolve(__dirname + '/perfectapi.json'), function(err, commandName, config, callback) {
-	if (err) return console.log('error: ' + err);
-	
-	console.log('Command called: ' + commandName);
-	
-	if (commandName == 'gen') {
-		gen.getImageUsingConfig(config, function(err, result) {
-			callback(err, result);
-		});
-	} else if (commandName == 'scripts') {
-		//todo
-	} else {
-		callback('Command ' + commandName + ' is not supported');
-	}
+var app = express.createServer();
 
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(perfectapi.restify(path.resolve(__dirname + '/perfectapi.json')));
 });
+
+app.post('/gen', function(req, res) {
+	var config = req.perfectapi.config;
+	
+	gen.getImageUsingConfig(config, function(err, result) {
+		if (err) 
+			res.end('Bad request - ' + err)
+		else
+			res.end(result);
+	});
+});
+
+app.post('/scripts', function(req, res) {
+	var config = req.perfectapi.config;
+	
+	res.end('Not supported yet');
+});
+
+app.listen(3000);
