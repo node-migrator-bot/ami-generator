cfb156c9d683fe4050c455e2c810b5fe396a137f
Steve Campbell <dukeytoo@gmail.com>
2012-02-05 10:57:31 -0600

    Add nodejs 0.4.9 version from chris lea packages

---
+++ scripts/ubuntu11.10/nodejs-0.4.9/user-data.sh
@@ -0,0 +1,19 @@
+apt-get install -y python-software-properties
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update -y
+sudo apt-get install -y nodejs=0.4.9-1ubuntu3
+
+cat >> /home/$MY_USER/readme.txt <<EOF
+
+Using Node
+==========
+
+To run a node program, type 
+$ node server.js
+...where server.js is the name of the node program
+
+To determine which version of node is setup, type
+$ node --version
+
+
+EOF
